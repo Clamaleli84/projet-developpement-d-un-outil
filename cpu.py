90732c1c761af1105b0a56df30d3051673700ff8
Clamaleli84 <leo.jaubert84@gmail.com>
2026-03-06 14:59:27 +0100

    Update cpu.py

--- cpu.py
+++ cpu.py
@@ -1,9 +1,5 @@
 import psutil
 import time
-while (True){
-  cpu = psutil.cpu_percent (interval=1)
+cpu = psutil.cpu_percent(interval=1)
+print(f"le CPU est utiliser à : {cpu}")
 
-  print(f"le CPU est utiliser à : {cpu}")
-
-  time.sleep(30)
-}
