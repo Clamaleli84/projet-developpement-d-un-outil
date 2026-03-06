3013538255ecece6b6e245d2e4f41e71282ddca5
Clamaleli84 <leo.jaubert84@gmail.com>
2026-03-06 15:15:06 +0100

    Update disque.sh

--- disque.sh
+++ disque.sh
@@ -1,2 +1,2 @@
 #/bin/bash
-df --total -h | awk '/^total/ {print "Espace disque disponible : " $4 "\nEspace disque utilisé : "$3","$5"}'
+df --total -h | awk '/^total/ {print "Espace disque disponible : " $4 "\nEspace disque utilisé : "$3","$5}'
