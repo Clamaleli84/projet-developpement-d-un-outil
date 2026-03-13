df7b281b8fc09eeb2f98b67782076fc1f60f1b0c
Clamaleli84 <leo.jaubert84@gmail.com>
2026-03-13 15:43:27 +0100

    Update parser.py

--- parser.py
+++ parser.py
@@ -9,21 +9,7 @@ def get_latest_cert_alert():
 
     for ligne in html.split("\n"):
         if "CERTFR" in ligne and "href" in ligne:
-            # Extraire l'URL
-            debut_url = ligne.find('href="') + 6
-            fin_url = ligne.find('"', debut_url)
-            alerte_url = "https://www.cert.ssi.gouv.fr" + ligne[debut_url:fin_url]
-            
-            # Extraire le titre → chercher le 2ème >
-            # Chercher le 3ème >
-            premier = ligne.find(">")
-            deuxieme = ligne.find(">", premier + 1)
-            troisieme = ligne.find(">", deuxieme + 1)
-            fin_titre = ligne.find("</a>")
-            titre = ligne[troisieme + 1:fin_titre].strip()
-            
-            print(f"URL : {alerte_url}")
-            print(f"Titre : '{titre}'")
+            print(f"LIGNE BRUTE : '{ligne}'")
             return
 
 get_latest_cert_alert()
