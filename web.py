1370532a37314cc91bc6bf82168b8f1eab4fa1bd
Clamaleli84 <leo.jaubert84@gmail.com>
2026-03-19 18:11:18 +0100

    Update web.py

--- web.py
+++ web.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<h1>Dashboard</h1>"
+    # Liste des machines correspondant à tes noms de fichiers
+    liste_machines = ["vm_locale", "ordi_local"]
+    return render_template("index.html", machines=liste_machines)
 
 if __name__ == "__main__":
+    # debug=True permet de voir les modifs sans relancer le script
     app.run(host="0.0.0.0", port=5000, debug=True)
