74b32425ff29824e7e4f4311f3945546b80ca561
Clamaleli84 <leo.jaubert84@gmail.com>
2026-03-19 17:37:12 +0100

    Update graphique.py

--- graphique.py
+++ graphique.py
@@ -1,22 +1,24 @@
 import pygal
 from storage import StorageManager
 
-def generer_graphique(sonde):
-    storage = StorageManager()
+MACHINES = ["vm_locale", "ordi_local"]
+
+def generer_graphique(sonde, nom_machine):
+    storage = StorageManager(db_path=f"metrics_{nom_machine}.db")
     rows = storage.get_history(sonde)
     if not rows:
-        print(f"Aucune donnée pour {sonde}")
+        print(f"Aucune donnée pour {sonde} sur {nom_machine}")
         return
     valeurs = [row[0] for row in rows]
     timestamps = [row[1] for row in rows]
     graphique = pygal.Line()
-    graphique.title = f"Historique {sonde}"
+    graphique.title = f"Historique {sonde} - {nom_machine}"
     graphique.x_labels = timestamps
     graphique.add(sonde, valeurs)
-    graphique.render_to_file(f"{sonde}.svg")
-    print(f"Graphique généré : {sonde}.png")
+    graphique.render_to_file(f"{sonde}_{nom_machine}.svg")
+    print(f"Graphique généré : {sonde}_{nom_machine}.svg")
 
-# Générer pour chaque sonde
-generer_graphique("cpu")
-generer_graphique("ram")
-generer_graphique("disque")
+for machine in MACHINES:
+    generer_graphique("cpu", machine)
+    generer_graphique("ram", machine)
+    generer_graphique("disque", machine)
