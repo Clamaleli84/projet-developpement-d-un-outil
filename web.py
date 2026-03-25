d5d1fd6e94fc899acf6a61e49728d9bb44f8c970
Clamaleli84 <leo.jaubert84@gmail.com>
2026-03-25 14:28:43 +0100

    Update web.py

--- web.py
+++ web.py
@@ -29,5 +29,4 @@ def index():
     return render_template("index.html", machines=machines, metriques=metriques)
 
 if __name__ == "__main__":
-    # On utilise le port 5000
     app.run(host="0.0.0.0", port=5000, debug=True)
